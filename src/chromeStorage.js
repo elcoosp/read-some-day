6eda15d37ab82c48a2b66705cf0c5669ad341eb7
Elcoosp <elcoosp@gmail.com>
2018-06-26 22:39:16 +0200

    Styles change

--- src/chromeStorage.js
+++ src/chromeStorage.js
@@ -1,5 +1,5 @@
-const db = {}
-const S = chrome.storage.sync
+const db = {},
+  S = chrome.storage.sync
 
 db.set = o =>
   new Promise((resolve, reject) =>
@@ -15,15 +15,13 @@ db.get = key =>
     })
   )
 
-db.push = o => {
-  const [key, data] = Object.entries(o)[0]
-  return new Promise((resolve, reject) =>
+db.push = key => data =>
+  new Promise((resolve, reject) =>
     db
       .get(key)
       .then((prevData, newArr = (prevData ? prevData : []).concat(data)) =>
         db.set({ [key]: newArr }).then(resolve)
       )
   )
-}
 
 export default db
