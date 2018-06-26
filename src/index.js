99d0fb06f5ab5b0db2fb04bebc9522456213900e
Elcoosp <elcoosp@gmail.com>
2018-06-26 22:36:34 +0200

    Create dummy app component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import db from './chromeStorage.js'
 
+const notEmpty = a => a.length > 0
+const ReadingCard = ({ url, title }) => (
+  <a href={url}>
+    <div>
+      <h1>{title}</h1>
+    </div>
+  </a>
+)
 class App extends React.Component {
+  state = { readings: [] }
+
+  set = ([key]) => val =>
+    this.setState(() => ({
+      [key]: val
+    }))
+
+  componentDidMount = () => this.getReadings()
+
+  getReadings = () => db.get('readings').then(this.set`readings`)
   render() {
-    return <div> App </div>
+    const { readings } = this.state
+    console.log(readings)
+
+    return (
+      <div>
+        <button onClick={this.getReadings}>Refresh</button>
+        {readings && notEmpty(readings) ? (
+          readings.map(r => <ReadingCard key={r.url} {...r} />)
+        ) : (
+          <div>No readings marked yet !</div>
+        )}
+      </div>
+    )
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
